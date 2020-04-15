cea0015a3d0f7c682ff413411e706d32f93b456e
Matthew Malecki <mmalecki4@gmail.com>
2020-04-15 16:12:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,10 +15,13 @@ def loop_message_n_times(message_string, int)
   end
 end
 
-#def output_array(message_string)
-#  counter = 0
-  
-#end
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
 
 #def return_string_array(message_string)
 #  counter = 0
