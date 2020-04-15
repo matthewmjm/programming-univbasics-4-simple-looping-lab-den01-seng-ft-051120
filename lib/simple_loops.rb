63ca11f73b519ad36faf6954cefb1415e9ce3b08
Matthew Malecki <mmalecki4@gmail.com>
2020-04-15 16:25:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,10 +25,9 @@ end
 
 def return_string_array(array)
   counter = 0
-  new_array = array
   while counter < array.length do
-    new_array = new_array[counter].to_s
+    array = array[counter].to_s
     counter += 1
   end
-  new_array
+  array
 end
