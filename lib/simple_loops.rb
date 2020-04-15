fd6c85013ca6b6e3fa01d5474a58e043d7d63459
Matthew Malecki <mmalecki4@gmail.com>
2020-04-15 16:09:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+def loop_message_five_times(message_string)
+  counter = 0
+  while counter < 5 do
+    puts message_string
+   counter += 1
+  end
+end
+
+def loop_message_n_times(message_string, int)
+  counter = 0
+  while counter < int do
+    puts message_string
+    counter += 1
+  end
+end
+
+#def output_array(message_string)
+#  counter = 0
+  
+#end
+
+#def return_string_array(message_string)
+#  counter = 0
+  
+#end
+
